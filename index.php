15ee4de265539f2a62a09267f5529eae92d4d816
Neil DeLillo <neil@ndelillo.com>
2019-04-03 22:15:26 -0400

    templates yaya

--- index.php
+++ index.php
@@ -4,8 +4,19 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new Slim\App;
 
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $templates = __DIR__ . '/templates/';
+    $cache = __DIR__ . '/tmp/views/';
+
+    $view = new Slim\Views\Twig($templates, compact('cache'));
+
+    return $view;
+};  
+
 $app->get('/', function ($request, $response) {
-    return 'hello world';
+    return $this->view->render($response, 'home.twig');
 });
 
 $app->run();
---
+++ tmp/views/d5/d5297ca9849d73c27b347212f1487c21adfa8de685e82fcf5c7248ee9f1263af.php
@@ -0,0 +1,52 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* home.twig */
+class __TwigTemplate_3712eb1fbf756423d41da0826c9991877621caab2974099a6571f5ec50d02bb0 extends \Twig\Template
+{
+    private $source;
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "Welcome Home.";
+    }
+
+    public function getTemplateName()
+    {
+        return "home.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "home.twig", "/Users/neil/Desktop/Projects/slim/templates/home.twig");
+    }
+}
